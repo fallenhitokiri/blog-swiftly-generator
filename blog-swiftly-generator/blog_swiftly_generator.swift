fa7e4588c66025039f2c480efe5d5fc1509d8a20
Timo Zimmermann <timo@screamingatmyscreen.com>
2018-10-30 20:36:22 +0100

    Initial Commit

---
+++ blog-swiftly-generator/blog_swiftly_generator.swift
@@ -0,0 +1,11 @@
+//
+//  blog_swiftly_generator.swift
+//  blog-swiftly-generator
+//
+//  Created by Timo Zimmermann on 30.10.18.
+//  Copyright © 2018 Timo Zimmermann. All rights reserved.
+//
+
+class blog_swiftly_generator {
+
+}
